8f561ad418836dedcdb3fc64e8f1dcfc41aaeb2a
vivekgoyal2110 <vivekgoyal107@gmail.com>
2025-11-27 10:20:06 +0530

    Selenium Webdriver practiced

--- src/test/java/Demo/demo.java
+++ src/test/java/Demo/demo.java
@@ -1,7 +1,13 @@
 package Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class demo {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.google.com");
+        Thread.sleep(2000);
+        driver.quit();
     }
 }
